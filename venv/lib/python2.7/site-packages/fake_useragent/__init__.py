26980eca17207d643b658ef182841ec259bee4f5
stamaimer <stamaimer@gmail.com>
2016-12-07 16:14:26 +0800

    initial commit

---
+++ venv/lib/python2.7/site-packages/fake_useragent/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import, unicode_literals
+
+from fake_useragent.errors import FakeUserAgentError  # noqa # isort:skop
+from fake_useragent.fake import UserAgent  # noqa # isort:skop
---
+++ venv/lib/python2.7/site-packages/fake_useragent/settings.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+import tempfile
+
+__version__ = '0.1.3'
+
+DB = os.path.join(tempfile.gettempdir(), 'fake_useragent_{version}.json'.format(  # noqa
+    version=__version__,
+))
+
+CACHE_SERVER = 'https://fake-useragent.herokuapp.com/browsers/' + __version__
+
+BROWSERS_STATS_PAGE = 'http://www.w3schools.com/browsers/browsers_stats.asp'
+
+BROWSER_BASE_PAGE = 'http://useragentstring.com/pages/useragentstring.php?name={browser}'  # noqa
+
+BROWSERS_COUNT_LIMIT = 50
+
+REPLACEMENTS = {
+    ' ': '',
+    '_': '',
+}
+
+SHORTCUTS = {
+    'internet explorer': 'internetexplorer',
+    'ie': 'internetexplorer',
+    'msie': 'internetexplorer',
+    'google': 'chrome',
+    'googlechrome': 'chrome',
+    'ff': 'firefox',
+}
+
+OVERRIDES = {
+    'IE': 'Internet Explorer',
+}
+
+HTTP_TIMEOUT = 5
+
+HTTP_RETRIES = 2
+
+HTTP_DELAY = 0.1
